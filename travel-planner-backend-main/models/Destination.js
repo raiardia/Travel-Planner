6d2c1ab312ce49c6fdd61dcf80c06f0c82fcb154
raiardia <ardiaanggreni@gmail.com>
2025-05-27 10:28:52 +0800

    delete

--- travel-planner-backend-main/models/Destination.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const destinationSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  date: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Destination', destinationSchema);
